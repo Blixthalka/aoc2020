f79ee29c33225bcd17a8188fc415db0cbee87ac3
Emil Nilsson <emil.nilsson@trustly.com>
2020-12-06 12:38:26 +0100

    day 1

---
+++ 01/solution01.erl
@@ -0,0 +1,37 @@
+-module(solution01).
+-export([part1/0, part2/0]).
+
+%% Part 1
+part1() -> search_part1(read_input()).
+
+search_part1([]) -> 0;
+search_part1([H | T]) -> search_part1(H, T) + search_part1(T).
+
+search_part1(_, []) -> 0;
+search_part1(V, [H | T]) ->
+    if V + H =:= 2020 -> V * H;
+       true -> search_part1(V, T)
+    end.
+
+%% Part 2
+part2() -> search_part2(read_input()).
+
+search_part2([]) -> 0;
+search_part2([H | T]) -> search_part2(H, T) + search_part2(T).
+
+search_part2(_, []) -> 0;
+search_part2(V, [H | T]) -> search_part2(V, H, T) + search_part2(V, T).
+
+search_part2(_, _, []) -> 0;
+search_part2(V1, V2, [H | T]) ->
+    if V1 + V2 + H =:= 2020 -> V1 * V2 * H;
+       true -> search_part2(V1, V2, T)
+    end.
+
+%% Common
+read_input() ->
+    {ok, Data} = file:read_file("input.txt"),
+    Strings = string:split(Data, "\n", all),
+    Pairs = [string:to_integer(X) || X <- Strings],
+    {Result, _} = lists:unzip(Pairs),
+    Result.
